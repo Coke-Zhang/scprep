75d0559f605d186846332ca44a6d5a87a2e96b8a
Scott Gigante <scottgigante@gmail.com>
2019-03-21 17:51:28 -0400

    greatly simplify lazyload

--- scprep/_lazyload.py
+++ scprep/_lazyload.py
@@ -1,39 +1,27 @@
 import importlib
 
 # Key:
-# { module : [{submodule: [method1, class1]}, method2, class2]}
-# module loads method2 and class2 when either of them is accessed
-# module.submodule exists from initialization, but only loads
-# module.submodule.method1 and module.submodule.method2 when
-# either of them is accessed.
+# { module : [{submodule1:[subsubmodule1, subsubmodule2]}, submodule2] }
+# each module loads submodules on initialization but is only imported
+# and loads methods/classes when these are accessed
 importspec = {
-    'matplotlib': [{'colors': ['is_color_like', 'to_rgba', 'Normalize',
-                               'ListedColormap', 'LinearSegmentedColormap',
-                               'LogNorm', 'SymLogNorm', 'PowerNorm'],
-                    'pyplot': ['scatter', 'rcParams', 'subplots',
-                               'hist', 'setp', 'close', 'show'],
-                    'animation': ['FuncAnimation'],
-                    'cm': ['tab10'],
-                    'axes': ['Axes'],
-                    'lines': ['Line2D'],
-                    'ticker': ['MaxNLocator'],
-                    'transforms': ['ScaledTranslation']},
-                   'get_backend', 'rc'],
-    'mpl_toolkits': [{'mplot3d': ['Axes3D']}],
-    'fcsparser': [{'api': ['ParserFeatureNotImplementedError', 'parse']}],
-    'rpy2': [{'robjects': [{'numpy2ri': ['activate', 'ri2py'],
-                            'packages': ['STAP'],
-                            'vectors': ['ListVector'],
-                            'conversion': ['ri2py']}],
-              'rinterface': ['NULL', 'RRuntimeWarning']}],
-    'h5py': ['File', 'Group', 'Dataset'],
-    'tables': ['open_file', 'File', 'Group', 'CArray']
+    'matplotlib': ['colors', 'pyplot', 'animation', 'cm',
+                   'axes', 'lines', 'ticker', 'transforms'],
+    'mpl_toolkits': ['mplot3d'],
+    'fcsparser': ['api'],
+    'rpy2': [{'robjects': ['numpy2ri', 'packages', 'vectors', 'conversion']},
+             'rinterface'],
+    'h5py': [],
+    'tables': []
 }
 
 
 class AliasModule(object):
 
-    def __init__(self, name, members):
+    def __init__(self, name, members=None):
+        if members is None:
+            members = []
+        # easy access to AliasModule members to avoid recursionerror
         self.__module_name__ = name
         self.__module_members__ = members
         # always import these members if they exist
@@ -42,18 +30,27 @@ class AliasModule(object):
             '__loader__', '__path__', '__doc__', '__package__']
         self.__loaded__ = False
         # create submodules
+        submodules = []
         for member in members:
             if isinstance(member, dict):
                 for submodule, submembers in member.items():
                     setattr(self, submodule, AliasModule(
                         "{}.{}".format(name, submodule), submembers))
+                    submodules.append(submodule)
+            else:
+                setattr(self, member, AliasModule(
+                        "{}.{}".format(name, member)))
+                submodules.append(member)
+        setattr(self, "__submodules__", submodules)
 
     def __getattribute__(self, attr):
         # easy access to AliasModule members to avoid recursionerror
         super_getattr = super().__getattribute__
-        if attr in (super_getattr("__module_members__") +
-                    super_getattr("__implicit_members__")):
-            # accessing a lazy loaded member
+        if attr in super_getattr("__submodules__"):
+            # accessing a submodule, return directly
+            return super_getattr(attr)
+        else:
+            # accessing an unknown member
             if not super_getattr("__loaded__"):
                 # module hasn't been imported yet
                 setattr(
@@ -61,9 +58,6 @@ class AliasModule(object):
                     importlib.import_module(super_getattr("__module_name__")))
             # access lazy loaded member from loaded module
             return getattr(super_getattr("__module__"), attr)
-        else:
-            # not a loaded member
-            return super_getattr(attr)
 
 
 # load required aliases into global namespace
